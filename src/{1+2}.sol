0039ee9ade14a56127ec41c50979be6262b55ff7
Mario <mario.ethmax@gmail.com>
2024-10-08 09:44:59 -0400

    test

--- src/{1+2}.sol
+++ src/{1+2}.sol
@@ -1,10 +1,8 @@
-contract Sss 
-
-{
+contract Sss {
     event Aa();
     function aB(uint256 a) external view {
 if (a == 5){
-    emit Aa;
+    emit Aa();
 }
     }
 }
--- src/{1+4}.sol
+++ src/{1+4}.sol
@@ -4,7 +4,7 @@ contract Sss
     event Aa();
     function aB(uint256 a) external view {
 if (a == 5){
-    emit Aa;
+    emit Aa();
 }
     }
 }
